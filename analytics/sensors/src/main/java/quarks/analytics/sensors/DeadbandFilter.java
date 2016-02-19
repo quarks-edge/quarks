65b049e577485a2bcd89d2531794297c797cc22e
Daniel J. Debrunner <debrunne@us.ibm.com>
2016-02-19 08:57:32 -0800

    Add initial code for analytics/sensors with a deadband filter

---
+++ analytics/sensors/src/main/java/quarks/analytics/sensors/DeadbandFilter.java
@@ -0,0 +1,63 @@
+package quarks.analytics.sensors;
+
+import java.util.concurrent.TimeUnit;
+
+import quarks.function.Function;
+import quarks.function.Predicate;
+
+public class DeadbandFilter<T, V> implements Predicate<T> {
+
+    private static final long serialVersionUID = 1L;
+
+    private final Function<T, V> valueFunction;
+    private final Predicate<V> deadbandFunction;
+    private final long period;
+    private final TimeUnit unit;
+
+    // Always send the first value.
+    private transient boolean outOfBand = true;
+
+    private transient long lastSend;
+    
+    public DeadbandFilter(Function<T, V> valueFunction, Predicate<V> deadbandFunction) {
+        this(valueFunction , deadbandFunction, 0, null);
+    }
+
+    public DeadbandFilter(Function<T, V> valueFunction, Predicate<V> deadbandFunction, long period, TimeUnit unit) {
+        this.valueFunction = valueFunction;
+        this.deadbandFunction = deadbandFunction;
+        this.period = period;
+        this.unit = unit;
+    }
+
+    @Override
+    public boolean test(final T t) {
+        final V value = valueFunction.apply(t);
+        boolean passTuple;
+        long now = 0;
+        if (!deadbandFunction.test(value)) {
+            outOfBand = true;
+            passTuple = true;
+        } else if (outOfBand) {
+            // When the value returns to being in-band
+            // send the in-band value.
+            outOfBand = false;
+            passTuple = true;
+        } else {
+        	passTuple = false;
+        	if (period != 0) {
+                now = System.currentTimeMillis();
+                long sinceLast = unit.convert(now - lastSend, TimeUnit.MILLISECONDS);
+                if (sinceLast > period)
+                     passTuple = true;
+        	}
+        }
+
+        if (passTuple && period != 0)
+            lastSend = now == 0 ? System.currentTimeMillis() : now;
+            
+        return passTuple;
+    }
+
+}
+
---
+++ analytics/sensors/src/main/java/quarks/analytics/sensors/Filters.java
@@ -0,0 +1,83 @@
+package quarks.analytics.sensors;
+
+import java.util.concurrent.TimeUnit;
+
+import quarks.function.Function;
+import quarks.function.Predicate;
+import quarks.topology.TStream;
+
+/**
+ * Filters aimed at sensors.
+ */
+public class Filters {
+
+    /**
+     * Deadband filter with maximum suppression time.
+     * 
+     * A filter that discards any tuples that are in the deadband, uninteresting to downstream consumers.
+     * <P>
+     * A tuple {@code t} is passed through the deadband filter if:
+     * <UL>
+     * <LI>
+     * {@code deadband.test(value.apply(t))} is false, that is the tuple's value is outside of the deadband
+     * </LI>
+     * <LI>
+     * OR {@code deadband.test(value.apply(t))} is true AND the last tuple's value was outside of the deadband.
+     * This corresponds to the first tuple's value inside the deadband after a period being outside it.
+     * </LI>
+     * <LI>
+     * OR it has been more than {@code maximumSuppression} seconds (in unit {@code unit}) 
+     * </LI>
+     * <LI>
+     * OR it is the first tuple (effectively the state of the filter starts as outside of the deadband).
+     * </LI>
+     * </UL>
+     * 
+     * @param <T>
+     * 
+     * @param stream Stream containing readings.
+     * @param value Function to obtain the tuple's value passed to the deadband function.
+     * @param deadband Function that defines the deadband.
+     * @param maximumSuppression Maximum amount of time to suppress values that are in the deadband.
+     * @param unit Unit for {@code maximumSuppression}.
+     * @return Filtered stream.
+     */
+    public static <T, V> TStream<T> deadband(TStream<T> stream, Function<T, V> value, Predicate<V> deadband,
+            long maximumSuppression, TimeUnit unit) {
+
+        return stream.filter(new DeadbandFilter<>(value, deadband, maximumSuppression, unit));
+    }
+    
+    /**
+     * Deadband filter.
+     * 
+     * A filter that discards any tuples that are in the deadband, uninteresting to downstream consumers.
+     * <P>
+     * A tuple {@code t} is passed through the deadband filter if:
+     * <UL>
+     * <LI>
+     * {@code deadband.test(value.apply(t))} is false, that is the value is outside of the deadband
+     * </LI>
+     * <LI>
+     * OR {@code deadband.test(value.apply(t))} is true and the last value was outside of the deadband.
+     * This corresponds to the first value inside the deadband after a period being outside it.
+     * </LI>
+     * <LI>
+     * OR it is the first tuple (effectively the state of the filter starts as outside of the deadband).
+     * </LI>
+     * </UL>
+     * 
+     * 
+     * @param <T> Tuple type.
+     * @param <V> Value type for the deadband function.
+     * 
+     * @param stream Stream containing readings.
+     * @param value Function to obtain the value passed to the deadband function.
+     * @param deadband Function that defines the deadband.
+     * @return Filtered stream.
+     */
+    public static <T, V> TStream<T> deadband(TStream<T> stream, Function<T, V> value, Predicate<V> deadband) {
+
+        return stream.filter(new DeadbandFilter<>(value, deadband));
+    }
+}
