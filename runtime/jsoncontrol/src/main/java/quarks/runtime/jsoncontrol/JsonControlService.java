9741c30333eb8fa8c88dcda71bfa93a4ed4f27d2
Daniel J. Debrunner <debrunne@us.ibm.com>
2016-02-25 15:33:11 -0800

    Initial empty framework for json control service

---
+++ runtime/jsoncontrol/src/main/java/quarks/runtime/jsoncontrol/JsonControlService.java
@@ -0,0 +1,12 @@
+/*
+# Licensed Materials - Property of IBM
+# Copyright IBM Corp. 2015, 2016 
+*/
+package quarks.runtime.jsoncontrol;
+
+import quarks.execution.services.ControlService;
+
+/**
+ */
+public abstract class JsonControlService implements ControlService {
+}
---
+++ runtime/jsoncontrol/src/test/java/quarks/test/runtime/jsoncontrol/JsonControlServiceTest.java
@@ -0,0 +1,9 @@
+/*
+# Licensed Materials - Property of IBM
+# Copyright IBM Corp. 2015, 2016 
+*/
+package quarks.test.runtime.jsoncontrol;
+
+public class JsonControlServiceTest {
+    
+}
